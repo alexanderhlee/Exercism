3166931ec5fec49bf2b214ffca85359c5f8f28d1
alexanderhlee <alexanderhlee@gmail.com>
2020-05-30 18:19:41 -0400

    Add Kindergarten exercise

---
+++ fsharp/KindergartenGarden.fs
@@ -0,0 +1,22 @@
+﻿module KindergartenGarden
+
+type Plant = Grass | Clover | Radishes | Violets | Dead
+
+let studentPlaces = "AABBCCDDEEFFGGHHIIJJKKLL"
+
+let matchPlant = function
+    | 'G' -> Plant.Grass
+    | 'C' -> Plant.Clover
+    | 'R' -> Plant.Radishes
+    | 'V' -> Plant.Violets
+    | _ -> Plant.Dead
+    
+let seedFinder (seeds: string) (cup: int) =
+    seeds.[cup..cup + 1]
+
+let plants (diagram: string) (student: string) =
+    let studentCup = studentPlaces.IndexOf(student.[0..0])
+    let seeds = diagram.Split[|'\n'|]
+    let studentSeeds = seedFinder seeds.[0] studentCup +
+                       seedFinder seeds.[1] studentCup
+    studentSeeds |> Seq.toList |> List.map matchPlant
